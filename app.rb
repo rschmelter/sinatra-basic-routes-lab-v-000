68668efe01ace63e80184dfc40d138e32c09cced
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-20 01:45:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,12 +4,12 @@ class App < Sinatra::Base
     get '/name' do
       "My name is _"
    end
-   
+
    get '/hometown' do
      "My hometown is _"
    end
 
-  get '/favorit-song' do
+  get '/favorite-song' do
     "My favorite song is _"
 
   end
